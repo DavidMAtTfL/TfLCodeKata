e4c6b7a814edac66e7a55c86fcb0297c8731ef48
DPMonaghan <davidmonaghan89@gmail.com>
2016-09-20 13:38:23 +0100

    Added bookshop code from kata session

---
+++ Bookshop/Bookshop/Bookshop/Bookstore.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookshop
+{
+    public class Bookstore
+    {
+        public List<Book> Books;
+        public double ShoppingCartBalance { get; set; }
+
+        public void Purchase(IList<string> bookNames )
+        {
+            var bookToPurchase = Books.Where(x => bookNames.Contains(x.Name));
+
+            ShoppingCartBalance = bookToPurchase.Sum(x => x.Price);
+        }
+    }
+}
