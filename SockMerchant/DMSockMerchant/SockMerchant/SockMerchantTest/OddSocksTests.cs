81a6e017f0fcd567f03cbe47cc240b6275c94673
DPMonaghan <davidmonaghan89@gmail.com>
2016-10-06 09:37:44 +0100

    Added code from kata session

---
+++ SockMerchant/DMSockMerchant/SockMerchant/SockMerchantTest/OddSocksTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SockMerchant;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SockMerchant.Tests
+{
+    [TestClass()]
+    public class OddSocksTests
+    {
+        [TestMethod()]
+        public void CanPair_CanPairToEmpty_ReturnFalse()
+        {
+            var sut = new OddSocks();
+
+            var result = sut.CanPair("red");
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void CanPair_CanPairWithSameColour_ReturnTrue()
+        {
+            var sut = new OddSocks();
+
+            sut.CanPair("red");
+            var result = sut.CanPair("red");
+
+            Assert.IsTrue(result);
+        }
+    }
+}
---
+++ SockMerchant/DMSockMerchant/SockMerchant/SockMerchantTest/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SockMerchant;
+
+namespace SockMerchantTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void OneSock_NotPaired()
+        {
+            var inputArray = new string[] { "red" };
+            var result = SockPairer.Process(inputArray);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void TwoSocksSameColour_Pair()
+        {
+            var inputArray = new string[] { "red", "red" };
+            var result = SockPairer.Process(inputArray);
+
+            Assert.AreEqual(1, result);
+        }
+    }
+}
