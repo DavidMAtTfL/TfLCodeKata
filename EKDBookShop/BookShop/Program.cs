5b0f11f36997befbfa961ed64f1cbcdae9c6ab28
DPMonaghan <davidmonaghan89@gmail.com>
2017-10-17 13:55:47 +0100

    added Eleanor, Kelly and Davids code

---
+++ EKDBookShop/BookShop/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cost = 0;
+            string input = string.Empty;
+            while(!input.ToLower().Equals("exit"))
+            {
+                Console.WriteLine("enter books");
+                input = Console.ReadLine();
+                if (input.Equals("book1"))
+                {
+                    cost = cost + 8;
+                }
+                if (input.Equals("book2"))
+                {
+                    cost = cost + 8;
+                }
+                Console.WriteLine("cost:" + cost);
+            }
+        }
+    }
+}
